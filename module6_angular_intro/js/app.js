0d18b024a2ebcdd7248755e571ce6fc4bb9969ab
1amBulletproof <brandon.tarney@gmail.com>
2018-03-10 15:05:43 -0500

    +basic fcn: tell user if they had too much

--- module6_angular_intro/js/app.js
+++ module6_angular_intro/js/app.js
@@ -6,18 +6,34 @@ angular.module('App', [])
 
 AppController.$inject = ['$scope'];
 function AppController($scope) {
-  $scope.userInput = "TEST";
-  $scope.messageToUser = "TEST";
+  $scope.userInput = "list comma separated dishes you usually have for lunch";
+  $scope.messageToUser = "";
 
   $scope.checkUserInput = function () {
     //TODO: Implement this method
-    var numberOfEntries = calculateNumberOfEntries($scope.userInput)
     $scope.messageToUser = "Checking User Input....";
+    var numberOfEntries = calculateNumberOfEntries($scope.userInput)
+    if (numberOfEntries > 0 && numberOfEntries <= 3) {
+      $scope.messageToUser = "Enjoy!"
+    } else if (numberOfEntries > 3) {
+      $scope.messageToUser = "Too much!"
+    } else if (numberOfEntries == 0) {
+      $scope.messageToUser = "Please enter data first"
+    } else {
+      $scope.messageToUser = "Whoops! There was an error"
+    }
+
   };
 
   function calculateNumberOfEntries(string) {
-      //TODO: Implement this method
-    return 0;
+    // console.log(string)
+    if (string == "") {
+      return 0
+    }
+    var splitString = string.split(',')
+    // console.log(splitString)
+    // console.log(splitString.length)
+    return splitString.length
   }
 
 }
