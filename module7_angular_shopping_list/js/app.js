73a34d023861954cccdeafc91990e0c2fb82e867
1amBulletproof <brandon.tarney@gmail.com>
2018-03-24 11:14:47 -0400

    +populate to-buy && already-bought lists & move from from the other with a button

--- module7_angular_shopping_list/js/app.js
+++ module7_angular_shopping_list/js/app.js
@@ -14,16 +14,25 @@
   function ToBuyController($scope, ShoppingListCheckoffService) {
     console.log('ToBuyController start')
     console.log($scope)
-    this.statement = "To Buy:"
+    this.title = "To Buy:"
+    this.toBuyItems = ShoppingListCheckoffService.getToBuyItems();
+    console.log(this.toBuyItems)
 
     console.log(ShoppingListCheckoffService)
+
+    this.buyAnItem = function(index) {
+      console.log("Bought item " + index)
+      //TODO: call ShoppingListCheckoffService.buyAnItem(index)
+      ShoppingListCheckoffService.buyAnItem(index)
+    }
   }
 
   AlreadyBoughtController.$inject = ['$scope', 'ShoppingListCheckoffService'];
   function AlreadyBoughtController($scope, ShoppingListCheckoffService) {
     console.log('AlreadyBoughtController start')
     console.log($scope)
-    this.statement = "Already Bought:"
+    this.title = "Already Bought:"
+    this.boughtItems = ShoppingListCheckoffService.getBoughtItems();
   }
 
   function ShoppingListCheckoffService() {
@@ -60,81 +69,12 @@
       return alreadyBoughtItems;
     }
 
-    this.buyAnItem = function(itemIdx) {
+    this.buyAnItem = function(toBuyItemIndex) {
+      console.log(toBuyItems[toBuyItemIndex])
+
+      alreadyBoughtItems.push(toBuyItems[toBuyItemIndex])
 
     }
   }
-
-
-
 
 })();
-
-
-
-/*
-  function CustomFilterFactory() {
-      return function (input) {
-        input = input || "";
-        input = input.replace("likes", "loves");
-        return input;
-      };
-    }
-
-
-  function TruthFilter() {
-    return function (input, target, replace) {
-      input = input || "";
-      input = input.replace(target, replace);
-      return input;
-    }
-  }
-
-
-//POPULATE LISTS?! - see lecture 17 ng-repeat
-(function () {
-'use strict';
-
-var shoppingList1 = [
-  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
-];
-
-var shoppingList2 = [
-  {
-    name: "Milk",
-    quantity: "2"
-  },
-  {
-    name: "Donuts",
-    quantity: "200"
-  },
-  {
-    name: "Cookies",
-    quantity: "300"
-  },
-  {
-    name: "Chocolate",
-    quantity: "5"
-  }
-];
-
-angular.module('ShoppingListApp', [])
-.controller('ShoppingListController', ShoppingListController);
-
-ShoppingListController.$inject = ['$scope'];
-function ShoppingListController($scope) {
-  $scope.shoppingList1 = shoppingList1;
-  $scope.shoppingList2 = shoppingList2;
-
-  $scope.addToList = function () {
-    var newItem = {
-      name: $scope.newItemName,
-      quantity: $scope.newItemQuantity
-    };
-
-    $scope.shoppingList2.push(newItem);
-  };
-}
-
-})();
-*/
